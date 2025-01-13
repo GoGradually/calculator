558e8473b0fc75668ef95c91d4b9c7b368290111
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-13 15:30:12 +0900

    feat: 기능 완성

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,6 @@
+import java.io.StringReader;
+import java.util.Scanner;
+
 public class Calculator {
     int add(int i, int j) {
         return i + j;
@@ -13,6 +16,42 @@ public class Calculator {
 
     int divide(int i, int j) {
         return i / j;
+    }
+
+    int calculate(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        String[] split = s.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].isEmpty()) {
+                split[i] = "0";
+            }
+        }
+        if (split.length % 2 == 0) {
+            throw new IllegalArgumentException("Invalid expression");
+        }
+        split[0] = transform(split[0]);
+        for (int i = 1; i < split.length; i += 2) {
+            split[i+1] = transform(split[i+1]);
+            split[i+1] = switch (split[i]) {
+                case "+" -> String.valueOf(add(Integer.parseInt(split[i - 1]), Integer.parseInt(split[i + 1])));
+                case "-" -> String.valueOf(subtract(Integer.parseInt(split[i - 1]), Integer.parseInt(split[i + 1])));
+                case "*" -> String.valueOf(multiply(Integer.parseInt(split[i - 1]), Integer.parseInt(split[i + 1])));
+                case "/" -> String.valueOf(divide(Integer.parseInt(split[i - 1]), Integer.parseInt(split[i + 1])));
+                default -> throw new IllegalArgumentException("Invalid expression");
+            };
+        }
+        return Integer.parseInt(split[split.length-1]);
+    }
+
+    private String transform(String s) {
+        String[] split = s.split("[,:]");
+        int sum = 0;
+        for (int i = 0; i < split.length; i++) {
+            sum += Integer.parseInt(split[i]);
+        }
+        return String.valueOf(sum);
     }
 
     public static void main(String[] args) {
